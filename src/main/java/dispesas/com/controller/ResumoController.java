020a1efc0453b965b1582db22fc1a97da1796c52
ryanmosc <ryanoliveiramosc.com.098@gmail.com>
2026-06-24 09:33:42 -0300

    Correção das rotas

--- src/main/java/dispesas/com/controller/ResumoController.java
+++ src/main/java/dispesas/com/controller/ResumoController.java
@@ -3,7 +3,6 @@ package dispesas.com.controller;
 import dispesas.com.dto.dashboardDto.ResumoCategoriaResponse;
 import dispesas.com.dto.dashboardDto.ResumoMensalResponse;
 import dispesas.com.dto.dashboardDto.SaldoResponse;
-import dispesas.com.dto.despesaDto.ComparativoMensalResponseDTO;
 import dispesas.com.dto.despesaDto.GastosPorMesDTO;
 import dispesas.com.dto.despesaDto.Top5CategoriasMesDTO;
 import dispesas.com.dto.despesaDto.Top5GastosMesDTO;
@@ -38,18 +37,18 @@ public class ResumoController {
         return ResponseEntity.ok(resumoService.resumoPorCategoria(mes, ano));
     }
 
-    @GetMapping("/resumo/saldo")
+    @GetMapping("/saldo")
     public ResponseEntity<SaldoResponse> saldoGeral() {
         return ResponseEntity.ok(resumoService.saldoGeral());
     }
 
-    @GetMapping("/resumo/relatorios/gastos-por-mes")
+    @GetMapping("/relatorios/gastos-por-mes")
     public ResponseEntity<List<GastosPorMesDTO>> gastoPorMes(){
         return ResponseEntity.ok(resumoService.listarGastoPorMes());
     }
 
 
-    @GetMapping("/resumo/top-5-gastos")
+    @GetMapping("/top-5-gastos")
     public ResponseEntity<List<Top5GastosMesDTO>> top5GastosMes(
             @RequestParam Integer mes,
             @RequestParam Integer ano
@@ -57,21 +56,11 @@ public class ResumoController {
         return ResponseEntity.ok(resumoService.listarTop5MaioresGastosMes(mes, ano));
     }
 
-    @GetMapping("/resumo/top-5-categorias")
+    @GetMapping("/top-5-categorias")
     public ResponseEntity<List<Top5CategoriasMesDTO>> top5CategoriasMes(
             @RequestParam Integer mes,
             @RequestParam Integer ano
     ) {
         return ResponseEntity.ok(resumoService.listarTop5CategoriasMes(mes, ano));
-    }
-
-    @GetMapping("/resumo/comparativo")
-    public ResponseEntity<ComparativoMensalResponseDTO> compararMeses(
-            @RequestParam Integer mesA,
-            @RequestParam Integer anoA,
-            @RequestParam Integer mesB,
-            @RequestParam Integer anoB
-    ) {
-        return ResponseEntity.ok(resumoService.compararMeses(mesA, anoA, mesB, anoB));
     }
 }
