e1e9a996885db54de2d82636e5995d885bd944ad
ryanmosc <ryanoliveiramosc.com.098@gmail.com>
2026-06-26 08:50:05 -0300

    Foi adicionado mais categorias de despesas

--- src/main/java/dispesas/com/model/enumModel/Category.java
+++ src/main/java/dispesas/com/model/enumModel/Category.java
@@ -1,6 +1,34 @@
 package dispesas.com.model.enumModel;
 
 public enum Category {
-    ALIMENTACAO, TRANSPORTE, SAUDE, LAZER,
-    MORADIA, EDUCACAO, VESTUARIO, OUTROS
+
+        ALIMENTACAO,
+        TRANSPORTE,
+        COMBUSTIVEL,
+        MORADIA,
+        AGUA,
+        ENERGIA,
+        INTERNET,
+        TELEFONIA,
+        GAS,
+        CONDOMINIO,
+        SAUDE,
+        FARMACIA,
+        EDUCACAO,
+        CUIDADOS_PESSOAIS,
+        CURSOS,
+        LAZER,
+        VIAGEM,
+        VESTUARIO,
+        PRESENTES,
+        PETS,
+        ASSINATURAS,
+        IMPOSTOS,
+        SEGUROS,
+        INVESTIMENTOS,
+        SALARIO,
+        MANUTENCAO_VEICULO,
+        ESCRITORIO,
+        OUTROS
+
 }
